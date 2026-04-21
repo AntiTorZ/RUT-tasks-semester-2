3adef0914ed15b9c9545ea9fbd001ea1058c2a4e
Садиков Артём ТКИ-142 <AntiTor5708@yandex.ru>
2026-04-21 13:46:14 +0300

    Create Point.cpp

---
+++ src/Point.cpp
@@ -0,0 +1,45 @@
+#include "../include/Point.h"
+#include <iostream>
+#include <iomanip>
+
+namespace miit::approximation {
+
+Point::Point(double x, double y) : x_(x), y_(y) {}
+
+double Point::getX() const { return x_; }
+double Point::getY() const { return y_; }
+
+bool Point::operator==(const Point& other) const {
+    return x_ == other.x_ && y_ == other.y_;
+}
+
+bool Point::operator!=(const Point& other) const {
+    return !(*this == other);
+}
+
+bool Point::operator<(const Point& other) const {
+    return x_ < other.x_;
+}
+
+bool Point::operator>(const Point& other) const {
+    return x_ > other.x_;
+}
+
+bool Point::operator<=(const Point& other) const {
+    return !(*this > other);
+}
+
+bool Point::operator>=(const Point& other) const {
+    return !(*this < other);
+}
+
+std::ostream& operator<<(std::ostream& os, const Point& p) {
+    os << std::fixed << std::setprecision(2) << "(" << p.x_ << ", " << p.y_ << ")";
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Point& p) {
+    return is >> p.x_ >> p.y_;
+}
+
+} // namespace miit::approximation
