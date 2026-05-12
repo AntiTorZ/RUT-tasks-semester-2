6d26fd1dd8d8b7e0696f06140363c603527bc0fb
Садиков Артём ТКИ-142 <AntiTor5708@yandex.ru>
2026-05-12 13:22:18 +0300

    Create ClassZeroOrderExtrapolator.h

---
+++ include/ClassZeroOrderExtrapolator.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include "ClassPiecewiseLinear.h"
+#include "ClassPoint.h"
+
+/**
+* @brief Класс "Отрезок" для экстраполятора нулевого порядка
+*/
+class Segment
+{
+private:
+    /**
+    * @param leftPoint - левая точка отрезка
+    */
+    Point leftPoint;
+
+    /**
+    * @param rightPoint - правая точка отрезка
+    *                   (x = x правой точки, y = y левой точки)
+    */
+    Point rightPoint;
+
+public:
+    /**
+    * @brief Конструктор отрезка по двум точкам
+    * @param left - левая точка
+    * @param right - правая точка (используется только её x)
+    */
+    Segment(const Point& left, const Point& right);
+
+    /**
+    * @brief Получает левую точку
+    * @return Левая точка
+    */
+    Point getLeftPoint() const;
+
+    /**
+    * @brief Получает правую точку
+    * @return Правая точка
+    */
+    Point getRightPoint() const;
+
+    /**
+    * @brief Проверяет, принадлежит ли x отрезку
+    * @param x - проверяемая абсцисса
+    * @return true если x в пределах отрезка
+    */
+    bool contains(double x) const;
+
+    /**
+    * @brief Оператор сдвига влево
+    * @param value - величина сдвига
+    * @return Новый сдвинутый отрезок
+    */
+    Segment operator << (double value) const;
+
+    /**
+    * @brief Статический метод чтения отрезка из потока
+    * @param is - поток ввода
+    * @return Прочитанный отрезок
+    */
+    static Segment read(istream& is);
+};
+
+/**
+* @brief Класс "Экстраполятор нулевого порядка"
+*/
+class ZeroOrderExtrapolator : public PiecewiseLinearApproximation
+{
+private:
+    /**
+    * @param segments - коллекция отрезков
+    */
+    vector<Segment> segments;
+
+    /**
+    * @brief Строит отрезки из точек
+    */
+    void buildSegments();
+
+public:
+    /**
+    * @brief Конструктор с вектором точек
+    * @param points - вектор точек
+    */
+    ZeroOrderExtrapolator(vector<Point>& points);
+
+    /**
+    * @brief Конструктор со списком инициализации
+    * @param points - список точек
+    */
+    ZeroOrderExtrapolator(initializer_list<Point> points);
+
+    /**
+    * @brief Метод расчёта ординаты по заданной абсциссе
+    * @param x - абсцисса
+    * @return Вычисленная ордината (кусочно-постоянная функция)
+    */
+    double calculateY(double x) const override;
+
+    /**
+    * @brief Оператор сдвига влево для всех отрезков
+    * @param value - величина сдвига
+    * @return Сдвинутый экстраполятор
+    */
+    ZeroOrderExtrapolator operator << (double value) const;
+
+    /**
+    * @brief Статический метод чтения отрезка
+    * @param is - поток ввода
+    * @return Прочитанный отрезок
+    */
+    static Segment readSegment(istream& is);
+};
