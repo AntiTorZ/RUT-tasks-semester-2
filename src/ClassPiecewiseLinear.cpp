13e90512c107b3d58eb0cb3d997199e539c05f42
Садиков Артём ТКИ-142 <AntiTor5708@yandex.ru>
2026-05-12 13:21:59 +0300

    Create ClassPiecewiseLinear.cpp

---
+++ src/ClassPiecewiseLinear.cpp
@@ -0,0 +1,34 @@
+#include "../include/ClassPiecewiseLinear.h"
+#include <algorithm>
+
+PiecewiseLinearApproximation::PiecewiseLinearApproximation(vector<Point>& points) : points(points) {
+    std::sort(this->points.begin(), this->points.end());
+}
+
+PiecewiseLinearApproximation::PiecewiseLinearApproximation(initializer_list<Point> points) : points(points) {
+    std::sort(this->points.begin(), this->points.end());
+}
+
+void PiecewiseLinearApproximation::addPoint(const Point& point) {
+    points.push_back(point);
+    std::sort(points.begin(), points.end());
+}
+
+vector<Point> PiecewiseLinearApproximation::getPoints() const {
+    return points;
+}
+
+vector<Point> PiecewiseLinearApproximation::readPoints(istream& is) {
+    vector<Point> pts;
+    int n;
+    cout << "Введите количество точек: ";
+    if (!(is >> n)) return pts;
+    
+    for (int i = 0; i < n; ++i) {
+        Point p;
+        cout << "Точка " << i + 1 << " (x y): ";
+        is >> p;
+        pts.push_back(p);
+    }
+    return pts;
+}
