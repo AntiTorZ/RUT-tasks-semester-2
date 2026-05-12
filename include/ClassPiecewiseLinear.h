a8b23c952add6067eae3f439dd5fb9077cbc7912
Садиков Артём ТКИ-142 <AntiTor5708@yandex.ru>
2026-05-12 13:22:30 +0300

    Create ClassPiecewiseLinear.h

---
+++ include/ClassPiecewiseLinear.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <vector>
+#include <initializer_list>
+#include <iostream>
+#include "ClassPoint.h"
+
+using namespace std;
+
+/**
+* @brief Абстрактный базовый класс "Кусочно-линейная аппроксимация функции"
+*/
+class PiecewiseLinearApproximation
+{
+protected:
+    /**
+    * @param points - коллекция точек, задающих функцию
+    */
+    vector<Point> points;
+
+public:
+    /**
+    * @brief Конструктор с вектором точек
+    * @param points - вектор точек
+    */
+    PiecewiseLinearApproximation(vector<Point>& points);
+
+    /**
+    * @brief Конструктор со списком инициализации
+    * @param points - список точек
+    */
+    PiecewiseLinearApproximation(initializer_list<Point> points);
+
+    /**
+    * @brief Виртуальный деструктор
+    */
+    virtual ~PiecewiseLinearApproximation() = default;
+
+    /**
+    * @brief Добавляет точку в коллекцию
+    * @param point - добавляемая точка
+    */
+    void addPoint(const Point& point);
+
+    /**
+    * @brief Получает коллекцию точек
+    * @return Вектор точек
+    */
+    vector<Point> getPoints() const;
+
+    /**
+    * @brief Виртуальный метод расчёта ординаты по заданной абсциссе
+    * @param x - абсцисса
+    * @return Вычисленная ордината
+    */
+    virtual double calculateY(double x) const = 0;
+
+    /**
+    * @brief Статический метод чтения точек из потока
+    * @param is - поток ввода
+    * @return Вектор прочитанных точек
+    */
+    static vector<Point> readPoints(istream& is);
+};
